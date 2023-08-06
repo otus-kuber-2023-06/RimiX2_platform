8f629dfb7d2e834938e1a09fabf11a344d75c3d6
Rim Ganiev <GanievRR@intech.rshb.ru>
2023-08-07 01:01:12 +0500

    add wa 2

--- kubernetes-operators/build/mysqloperator.py
+++ kubernetes-operators/build/mysqloperator.py
@@ -2,6 +2,7 @@ import kopf
 import yaml
 import kubernetes
 import time
+import logging
 from jinja2 import Environment, FileSystemLoader
 
 
@@ -13,12 +14,17 @@ def render_template(filename, vars_dict):
     return json_manifest
 
 
+@kopf.on.delete("persistence.volume.claim")
+def notify(body, spec, **kwargs):
+    logging.info(f"Deleting {body['metadata']['name']}")
+
+
 @kopf.on.create("otus.homework", "v1", "mysqls")
 # Функция, которая будет запускаться при создании объектов тип MySQL:
 def mysql_on_create(body, spec, **kwargs):
     # cохраняем в переменные содержимое описания MySQL из CR
     name = body["metadata"]["name"]
-    image = body["spec"]["image"]  
+    image = body["spec"]["image"]
     password = body["spec"]["password"]
     database = body["spec"]["database"]
     storage_size = body["spec"]["storage_size"]
@@ -36,24 +42,36 @@ def mysql_on_create(body, spec, **kwargs):
         "mysql-deployment.yml.j2",
         {"name": name, "image": image, "password": password, "database": database},
     )
-    
+
+    # kopf.adopt(persistent_volume)
+    # kopf.adopt(persistent_volume_claim)
+    # kopf.adopt(service)
+    # kopf.adopt(deployment)
+
     kopf.append_owner_reference(persistent_volume, owner=body)
-    kopf.append_owner_reference(persistent_volume_claim, owner=body) # addopt
+    kopf.append_owner_reference(persistent_volume_claim, owner=body)
     kopf.append_owner_reference(service, owner=body)
     kopf.append_owner_reference(deployment, owner=body)
 
     api = kubernetes.client.CoreV1Api()
+
+    pv_list = api.list_persistent_volume()
+    if f"{name}-pv" in pv_list:
+        api.delete_persistent_volume(f"{name}-pv")
+        # Создаем mysql PV:
+        api.create_persistent_volume(persistent_volume)
+
+    # try:
     # Создаем mysql PV:
-    try: 
-        print(api.create_persistent_volume(persistent_volume))
-    except kubernetes.cliemt.exceptions.ApiException as e:
-        if e.status == 409:
-            pass
-        else:
-            raise e
+    #     print(api.create_persistent_volume(persistent_volume))
+    # except kubernetes.client.exceptions.ApiException as e:
+    #     if e.status == 409:
+    #         pass
+    #     else:
+    #         raise e
+
     # Создаем mysql PVC:
-    api.create_namespaced_persistent_volume_claim(namespace,
-    persistent_volume_claim)
+    api.create_namespaced_persistent_volume_claim(namespace, persistent_volume_claim)
     # Создаем mysql SVC:
     api.create_namespaced_service(namespace, service)
     # Создаем mysql Deployment:
@@ -61,7 +79,6 @@ def mysql_on_create(body, spec, **kwargs):
     api.create_namespaced_deployment(namespace, deployment)
 
 
-
-@kopf.on.delete('otus.homework', 'v1', 'mysqls')
+@kopf.on.delete("otus.homework", "v1", "mysqls")
 def delete_object_make_backup(body, **kwargs):
-    return {'message': "mysql and its children resources deleted"}
+    return {"message": "mysql and its children resources deleted"}
