6d20a16cdca4931801c0316cee3887b298c2bf2d
Rim Ganiev <GanievRR@intech.rshb.ru>
2023-08-07 02:26:12 +0500

    add backup and restore

--- kubernetes-operators/build/mysqloperator.py
+++ kubernetes-operators/build/mysqloperator.py
@@ -42,34 +42,43 @@ def mysql_on_create(body, spec, **kwargs):
         "mysql-deployment.yml.j2",
         {"name": name, "image": image, "password": password, "database": database},
     )
-
+    restore_job = render_template('restore-job.yml.j2', {
+        'name': name,
+        'image': image,
+        'password': password,
+        'database': database})
+    
     # kopf.adopt(persistent_volume)
     # kopf.adopt(persistent_volume_claim)
     # kopf.adopt(service)
     # kopf.adopt(deployment)
 
-    kopf.append_owner_reference(persistent_volume, owner=body)
+    # kopf.append_owner_reference(persistent_volume, owner=body)
     kopf.append_owner_reference(persistent_volume_claim, owner=body)
     kopf.append_owner_reference(service, owner=body)
     kopf.append_owner_reference(deployment, owner=body)
+    kopf.append_owner_reference(restore_job, owner=body)
 
-    api = kubernetes.client.CoreV1Api()
+    api_coreV1 = kubernetes.client.CoreV1Api()
 
-    
-    if any(pv.metadata.name == f'{name}-pv' for pv in api.list_persistent_volume().items):
-        print("bang!!!")
-
-    pv_list_1 = api.list_persistent_volume(field_selector = f'metadata.name={name}-pv')
-    if len(pv_list_1.items)==1 :
+    if any(
+        pv.metadata.name == f"{name}-pv"
+        for pv in api_coreV1.list_persistent_volume().items
+    ):
         logging.info(f"PV {name}-pv already exists. Deleting ...")
-        result = api.delete_persistent_volume(f"{name}-pv")
+        api_coreV1.delete_persistent_volume(f"{name}-pv")
+
+    # pv_list_1 = api.list_persistent_volume(field_selector = f'metadata.name={name}-pv')
+    # if len(pv_list_1.items)==1 :
+    #     logging.info(f"PV {name}-pv already exists. Deleting ...")
+    #     api.delete_persistent_volume(f"{name}-pv")
 
     # Создаем mysql PV:
-    api.create_persistent_volume(persistent_volume)
+    api_coreV1.create_persistent_volume(persistent_volume)
 
     # try:
     # Создаем mysql PV:
-    #     print(api.create_persistent_volume(persistent_volume))
+    #     logging.info(api.create_persistent_volume(persistent_volume))
     # except kubernetes.client.exceptions.ApiException as e:
     #     if e.status == 409:
     #         pass
@@ -77,28 +86,80 @@ def mysql_on_create(body, spec, **kwargs):
     #         raise e
 
     # Создаем mysql PVC:
-    api.create_namespaced_persistent_volume_claim(namespace, persistent_volume_claim)
+    api_coreV1.create_namespaced_persistent_volume_claim(
+        namespace, persistent_volume_claim
+    )
     # Создаем mysql SVC:
-    api.create_namespaced_service(namespace, service)
+    api_coreV1.create_namespaced_service(namespace, service)
     # Создаем mysql Deployment:
-    api = kubernetes.client.AppsV1Api()
-    api.create_namespaced_deployment(namespace, deployment)
+    api_appsV1 = kubernetes.client.AppsV1Api()
+    api_appsV1.create_namespaced_deployment(namespace, deployment)
+
+    # Пытаемся восстановиться из backup
+    try:
+        api_batchV1 = kubernetes.client.BatchV1Api()
+        api_batchV1.create_namespaced_job('default', restore_job)
+    except kubernetes.client.rest.ApiException:
+        pass
 
     # Cоздаем PVC и PV для бэкапов:
     try:
-        backup_pv = render_template('backup-pv.yml.j2', {'name': name})
-        api = kubernetes.client.CoreV1Api()
-        api.create_persistent_volume(backup_pv)
-    except kubernetes.client.rest.ApiException:
+        backup_pv = render_template("backup-pv.yml.j2", {"name": name})
+        api_coreV1.create_persistent_volume(backup_pv)
+    except kubernetes.client.exceptions.ApiException:
         pass
     try:
-        backup_pvc = render_template('backup-pvc.yml.j2', {'name': name})
-        api = kubernetes.client.CoreV1Api()
-        api.create_namespaced_persistent_volume_claim(namespace, backup_pvc)
-    except kubernetes.client.rest.ApiException:
-        pass    
+        backup_pvc = render_template("backup-pvc.yml.j2", {"name": name})
+        api_coreV1.create_namespaced_persistent_volume_claim(namespace, backup_pvc)
+    except kubernetes.client.exceptions.ApiException:
+        pass
 
+
+def delete_success_jobs(instance_name, ns):
+    logging.info("start deletion")
+    api = kubernetes.client.BatchV1Api()
+    jobs = api.list_namespaced_job(ns)
+    for job in jobs.items:
+        jobname = job.metadata.name
+        if (jobname == f"backup-{instance_name}-job") or (
+            jobname == f"restore-{instance_name}-job"
+        ):
+            if job.status.succeeded == 1:
+                api.delete_namespaced_job(
+                    jobname, ns, propagation_policy="Background"
+                )
+
+def wait_until_job_end(jobname, ns):
+    api_batchV1 = kubernetes.client.BatchV1Api()
+    job_finished = False
+    jobs = api_batchV1.list_namespaced_job(ns)
+    while (not job_finished) and \
+            any(job.metadata.name == jobname for job in jobs.items):
+        time.sleep(1)
+        jobs = api_batchV1.list_namespaced_job(ns)
+        for job in jobs.items:
+            if job.metadata.name == jobname:
+                logging.info(f"job with { jobname }  found,wait untill end")
+                if job.status.succeeded == 1:
+                    logging.info(f"job with { jobname } success")
+                    job_finished = True
 
 @kopf.on.delete("otus.homework", "v1", "mysqls")
 def delete_object_make_backup(body, **kwargs):
+    name = body["metadata"]["name"]
+    image = body["spec"]["image"]
+    password = body["spec"]["password"]
+    database = body["spec"]["database"]
+    namespace = body["metadata"]["namespace"]
+
+    delete_success_jobs(name, namespace)
+
+    # Cоздаем backup job:
+    api_batchV1 = kubernetes.client.BatchV1Api()
+    backup_job = render_template(
+        "backup-job.yml.j2",
+        {"name": name, "image": image, "password": password, "database": database},
+    )
+    api_batchV1.create_namespaced_job(namespace, backup_job)
+    wait_until_job_end(f"backup-{name}-job", namespace)
     return {"message": "mysql and its children resources deleted"}
