ba9be0743169da1c8cd555e88961e86897129387
Rim Ganiev <GanievRR@intech.rshb.ru>
2023-08-07 08:30:18 +0500

    patch reload.sh

--- kubernetes-operators/deploy/reload.sh
+++ kubernetes-operators/deploy/reload.sh
@@ -3,5 +3,9 @@ kubectl delete deployment mysql-instance
 kubectl delete pvc mysql-instance-pvc
 kubectl delete pv mysql-instance-pv
 kubectl delete svc mysql-instance
+kubectl delete job/backup-mysql-instance-job
+kubectl delete pvc/backup-mysql-instance-pvc
+kubectl delete pv/backup-mysql-instance-pv
+kubectl delete deployment/mysql-operator
 kubectl delete -f ../deploy/cr.yml
-kubectl apply -f ../deploy/cr.yml
+#kubectl apply -f ../deploy/cr.yml
